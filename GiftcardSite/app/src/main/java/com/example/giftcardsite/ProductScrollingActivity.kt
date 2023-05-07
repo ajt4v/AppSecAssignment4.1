289476410460c6975fcc60d60b97379160f0f85d
ajt4v <31350842+ajt4v@users.noreply.github.com>
2023-05-07 15:28:56 -0700

    fixing 5.2, 2.2.1, 5.1

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/ProductScrollingActivity.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/ProductScrollingActivity.kt
@@ -4,13 +4,6 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
